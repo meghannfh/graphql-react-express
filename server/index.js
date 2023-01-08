2fd6084d686effe4e1164773240e142c9d0e18f1
meghann <meghannfh@gmail.com>
2023-01-07 22:12:29 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const colors = require('colors')
+require('dotenv').config()
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const connectDB = require('./config/db')
+
+const PORT = process.env.PORT
+
+
+const app = express()
+
+//connect to database
+connectDB()
+
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(PORT, console.log(`server running on ${PORT}`))
---
+++ server/models/Client.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+//mongo schema is NOT related to a graphql schema
+const ClientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+});
+
+module.exports = mongoose.model('Client', ClientSchema)
